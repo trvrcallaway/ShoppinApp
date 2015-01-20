7d998998c6bae6c803dccc8fccd905b5f39fa170
trvrcallaway <trevor@stjoeweb.com>
2015-01-20 16:31:43 -0600

    Jan 20 2

--- js/function.js
+++ js/function.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
 
+  $('#active').keydown(function (e){
+    if(e.keyCode == 13){
+      event.preventDefault();
+      $('#add').click();
+    }
+  })
+
   $('#add').click(function(event) {
       var txtbox = document.getElementById('active');
       var txtval = txtbox.value;
       event.preventDefault();
 
-      $('<li class="items"></li>').appendTo('#list').html('<input type="checkbox"><span>' + txtval + '</span><button type="button" class="fa fa-trash delete"></button>');
+      if(!$.trim($('#active').val())) {
+        alert('Enter something, man.');
+      } else {
+
+      $('<li class="items"></li>').appendTo('#list').html('<input type="checkbox"> <span>' + txtval + '</span><button type="button" class="fa fa-trash delete"></button>');
 
       document.getElementById('active').value = '';
-  })
+      };
+    });
 
   $('#list').on('click', '.fa-trash', function(e){e.preventDefault(); $(this).parent().remove()});
 
